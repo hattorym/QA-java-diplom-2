ca552530c92cf664551325c96ff9a85393cb926a
hattorym <qa.iam.sab@gmail.com>
2023-03-19 01:02:57 +0300

    Add User & UserRandomizer class

---
+++ src/main/java/user/UserRandomizer.java
@@ -0,0 +1,18 @@
+package user;
+
+import com.github.javafaker.Faker;
+import io.qameta.allure.Step;
+
+public class UserRandomizer {
+
+    static Faker faker = new Faker();
+
+    @Step("Создание нового пользователя с рандомными данными")
+    public static User createNewRandomUser() {
+        return new User(
+                faker.name().firstName(),
+                faker.internet().emailAddress(),
+                faker.internet().password());
+    }
+
+}
