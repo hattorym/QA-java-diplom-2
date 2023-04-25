e4e796f05a154e3eaf4329243515f21a24ec8312
hattorym <qa.iam.sab@gmail.com>
2023-04-25 17:19:19 +0300

    Modified Order class

--- src/main/java/order/Order.java
+++ src/main/java/order/Order.java
@@ -1,32 +1,20 @@
 package order;
 
-import java.util.List;
+import ingredients.Ingredient;
 
 public class Order {
-    private List<String> ingredients;
+    private String[] ingredients;
+    private String _id;
+    private String status;
+    private String number;
 
-    public Order(List<String> ingredients) {
-        this.ingredients = ingredients;
+    public Order() {
     }
 
-    public static Order getOrderCorrectHash() {
-        return new Order(List.of("61c0c5a71d1f82001bdaaa6d", "61c0c5a71d1f82001bdaaa6f"));
-    }
-
-    public static Order getOrderNotCorrectHash() {
-        return new Order(List.of("7777777777", "8888888888"));
-    }
-
-    public static Order getOrderEmptyList() {
-        return new Order(List.of());
-    }
-
-    public List<String> getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(List<String> ingredients) {
-        this.ingredients = ingredients;
+    public Order(Ingredient ingredient) {
+        this.ingredients = new String[]{
+                ingredient.get_id()
+        };
     }
 
 }
